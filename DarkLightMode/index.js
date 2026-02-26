77e70ec4c5ff7230896dc90e5a856b340262b5b9
wd-heeyeonyoon <heeyeon.yoon@workday.com>
2026-02-26 16:59:53 +0000

    Add comments for ImageToggleButton project

--- DarkLightMode/index.js
+++ DarkLightMode/index.js
@@ -1,6 +1,7 @@
 let toggleBtn = document.querySelector(".toggle-btn input");
 let body = document.body;
 
+// When the toggle button is changed, the body class is toggled between dark-mode and light-mode
 toggleBtn.addEventListener("change", () => {
   body.classList.toggle("dark-mode");
 });
