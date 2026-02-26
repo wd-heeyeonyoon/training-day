43232a49dde2e98ce0719ebfc057072b59e12a13
wd-heeyeonyoon <heeyeon.yoon@workday.com>
2026-02-26 15:42:08 +0000

    Add ImageToggleButton project

---
+++ ImageToggleButton/index.js
@@ -0,0 +1,17 @@
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleImg = document.querySelector("#toggle-img");
+
+toggleBtn.addEventListener("click", () => {
+  // check if the image is currently hidden (state: before user clicks the button)
+  const isHidden = toggleImg.classList.contains("hidden");
+
+  if (isHidden) {
+    toggleImg.classList.remove("hidden");
+    toggleBtn.classList.remove("hidden");
+    toggleBtn.textContent = "Hide Image";
+  } else {
+    toggleImg.classList.add("hidden");
+    toggleBtn.classList.add("hidden");
+    toggleBtn.textContent = "Show Image";
+  }
+});
